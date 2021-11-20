312074b179d32d90f2c856f725021e8ae1e62345
maqing <136518041@qq.com>
2021-11-20 17:02:37 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+  import SearchInput from './components/input.vue'
+  export default {
+    components:{
+      SearchInput
+    }
+  }
+</script>
+
+<template>
+  <SearchInput/>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/input.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      <input type="text" />
+    </div>
+    <div>
+      <span>搜索</span>
+      <span>取消</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'input'
+}
+</script>
+
+<style scoped>
+
+</style>
